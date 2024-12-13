a7c99ae381102e66ef621d583b61d50d7772fdc3
YasmeenH2001 <jamria2001@gmail.com>
2024-12-13 18:39:54 +0300

    added

--- uob_student_nationalities.js
+++ uob_student_nationalities.js
@@ -1,1 +1,29 @@
+const url="https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"
 
+async function fetchUOBData() {
+    try{
+const response= await fetch(url)
+if (!response.ok || response.status !==200){
+    console.error('not ok')}
+    const data= await response.json();
+    displayUOBData(data.results)
+    }
+catch(error){
+    console.error('error occured', error);
+}}
+function displayUOBData(results){
+    const tableBody=document.getElementById('table-body')
+    results.forEach(results=>{
+const tableRow = document.createElement('tr')
+tableRow.innerHTML= `
+<td>${results.year}</td>
+<td>${results.semester}</td>
+<td>${results.the_programs}</td>
+<td>${results.nationality}</td>
+<td>${results.colleges}</td>
+<td>${results.number_of_students}</td>
+`
+tableBody.appendChild(tableRow)
+    });
+}
+document.addEventListener('DOMContentLoaded',fetchUOBData)
